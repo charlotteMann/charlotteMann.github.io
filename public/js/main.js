f0fa5f24ef7d7e61a9a9b21f309284ba4462b3c2
Marziyeh Ahmed <marziyeh.naaz.ahmed@gmail.com>
2016-02-16 15:05:35 +0000

    dynamically change stylesheet

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,7 @@
 
     document.getElementById('click-link').onclick = function(){
         document.getElementById('click-container').style.display="none";
-
+        document.getElementById('stylesheet').href="public/css/images.css";
         makeFullscreen();
         $(document).blur();
     };
