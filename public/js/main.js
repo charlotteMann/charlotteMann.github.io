af2d6743dbe306e1c4aa0b4dcb4a0e235c05b2bf
Marziyeh Ahmed <marziyehahmed@Marziyehs-MBP.lan>
2016-03-08 23:47:01 +0000

    faster

--- public/js/main.js
+++ public/js/main.js
@@ -7,27 +7,7 @@ document.getElementById('click-link').onclick = function(){
     makeFullscreen();
     $(document).blur();
 
-    var i=2;
-
-    changeImg(1);
-
-    setInterval(
-      function(){
-        if(i<=4){
-          changeImg(i++);
-          console.log(i);
-        } else {
-            i=1;
-            changeImg(i);
-            console.log(i);
-        }
-      }, 3000);
-
-    function changeImg(i){
-      // document.getElementById("image-slide").src = "./images/img"+i+".jpg";
-      document.body.style.backgroundImage="url('./images/img"+i+".jpg')";
-    }
-};
+  }
 
 // setInterval(
 //   function(){
